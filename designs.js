434d89f5841d703898b25ff730a7de9b6d562004
hussam soufan <hussam30190@gmail.com>
2022-05-16 12:38:05 +0200

    feat : Select submit

--- designs.js
+++ designs.js
@@ -4,7 +4,7 @@ let color = document.querySelector("#colorPicker");
 let height = document.querySelector("#inputHeight");
 let width = document.querySelector("#inputWidth");
 // When size is submitted by the user, call makeGrid()
-
+let submit = document.querySelector("#sizePicker input[type= submit]");
 function makeGrid() {
 
 // Your code goes here!
