7a4dc0d3f775a4373fd900f8e4050bbf3d482321
hussam soufan <hussam30190@gmail.com>
2022-05-16 12:23:54 +0200

    feat : Select sizes

--- designs.js
+++ designs.js
@@ -1,7 +1,8 @@
 // Select color input
 let color = document.querySelector("#colorPicker");
 // Select size input
-
+let height = document.querySelector("#inputHeight");
+let width = document.querySelector("#inputWidth");
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
