e8cda0e1bf3f623154801cb39df96a9992d9bf9b
hussam soufan <hussam30190@gmail.com>
2022-05-16 12:54:47 +0200

    feat : Draw the canvas

--- designs.js
+++ designs.js
@@ -5,8 +5,19 @@ let height = document.querySelector("#inputHeight");
 let width = document.querySelector("#inputWidth");
 // When size is submitted by the user, call makeGrid()
 let submit = document.querySelector("#sizePicker input[type= submit]");
-function makeGrid() {
+submit.addEventListener("click",makeGrid);
+let table = document.querySelector("#pixelCanvas");
 
-// Your code goes here!
-
+function makeGrid(event) {
+    table.innerHTML="";
+    event.preventDefault();
+    console.log("clicked");
+    for(let i= 0;i< Number(height.value);i++){
+        let tr = document.createElement("tr");
+        for(let j= 0;j< Number(width.value);j++){
+            let td = document.createElement("td");
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
